b11cee9b089872fe15ed420244f94018694e019d
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2022-08-31 09:16:01 -0300

    Agregar archivos de proyecto.

---
+++ frontend/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace frontend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOperando1 = new System.Windows.Forms.TextBox();
+            this.txtOperando2 = new System.Windows.Forms.TextBox();
+            this.txtOperacion = new System.Windows.Forms.TextBox();
+            this.btResultado = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtOperando1
+            // 
+            this.txtOperando1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOperando1.Location = new System.Drawing.Point(28, 26);
+            this.txtOperando1.Name = "txtOperando1";
+            this.txtOperando1.Size = new System.Drawing.Size(100, 29);
+            this.txtOperando1.TabIndex = 0;
+            // 
+            // txtOperando2
+            // 
+            this.txtOperando2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOperando2.Location = new System.Drawing.Point(220, 26);
+            this.txtOperando2.Name = "txtOperando2";
+            this.txtOperando2.Size = new System.Drawing.Size(100, 29);
+            this.txtOperando2.TabIndex = 1;
+            // 
+            // txtOperacion
+            // 
+            this.txtOperacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOperacion.Location = new System.Drawing.Point(155, 26);
+            this.txtOperacion.Name = "txtOperacion";
+            this.txtOperacion.Size = new System.Drawing.Size(43, 32);
+            this.txtOperacion.TabIndex = 2;
+            // 
+            // btResultado
+            // 
+            this.btResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btResultado.Location = new System.Drawing.Point(342, 25);
+            this.btResultado.Name = "btResultado";
+            this.btResultado.Size = new System.Drawing.Size(45, 33);
+            this.btResultado.TabIndex = 3;
+            this.btResultado.Text = "=";
+            this.btResultado.UseVisualStyleBackColor = true;
+            this.btResultado.Click += new System.EventHandler(this.btResultado_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(397, 37);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 24);
+            this.lblResultado.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 90);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btResultado);
+            this.Controls.Add(this.txtOperacion);
+            this.Controls.Add(this.txtOperando2);
+            this.Controls.Add(this.txtOperando1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOperando1;
+        private System.Windows.Forms.TextBox txtOperando2;
+        private System.Windows.Forms.TextBox txtOperacion;
+        private System.Windows.Forms.Button btResultado;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ frontend/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frontend
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btResultado_Click(object sender, EventArgs e)
+        {
+            if (txtOperacion.Text == "+")
+            {
+                //relizar la operacion
+                double oper1 = System.Convert.ToDouble(txtOperando1.Text);
+                double oper2 = System.Convert.ToDouble(txtOperando2.Text);
+
+                double res = oper1 + oper2;
+
+                //lblResultado.Text = res.ToString();
+                lblResultado.Text = System.Convert.ToString(res);
+            }
+            else if (txtOperacion.Text == "*")
+            {
+                //relizar la operacion
+                double oper1 = System.Convert.ToDouble(txtOperando1.Text);
+                double oper2 = System.Convert.ToDouble(txtOperando2.Text);
+
+                double res = oper1 * oper2;
+
+                //lblResultado.Text = res.ToString();
+                lblResultado.Text = System.Convert.ToString(res);
+            }
+            else
+            {
+                lblResultado.Text = "ERROR";
+            }
+        }
+    }
+}
